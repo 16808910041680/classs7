06569e286ea157c3fe8723651c3bcd2644f0eb46
16808910041680 <forforfirefire@gmail.com>
2025-04-13 11:11:28 +1000

    gu

---
+++ aibuho.py
@@ -0,0 +1,33 @@
+a = input ("Are you sick. Type Y for yes, N for no.")
+b = input ("What is your student 2 digit code")
+if a == "N":
+    if b > "10" and b < "20":
+       print ("Welcome, Whoever Youare. Please take a seat.")
+    elif b > "20" and b < "45":
+        c = input (("Welcome, Mr. You have enabled 2 step verification. Please type in your dog's name"))
+        if c == "Yuri":
+            print ("Hello, Arthur. Please take a seat.")
+        else:
+            print ("You are an impersonator. Please take a seat, because I bet you have Asian parents. This exam is the medical degree exam. You need a 100 to pass. Now, guards, please escort this man into the exam room.")
+    elif b > "45" and b < "70":
+        print ("You are an impersonator. Please take a seat, because I bet you have Asian parents. This exam is the medical degree exam. You need a 100 to pass. Now, guards, please escort this man into the exam room.")
+    elif b > "70" and b < "86":
+        d = input(("We have sent you an email. Please verify your number. Type in the 4 digit number that was sent."))
+        if d == "1049": 
+            print ("Hello, Solomon. Please take a seat.")
+        else:
+             print ("You are an impersonator. Please take a seat, because I bet you have Asian parents. This exam is the medical degree exam. You need a 100 to pass. Now, guards, please escort this man into the exam room.")
+    elif b > "86" and b < "100":
+        ("Welcome, Ms. Pleasetake. A. Seat.")
+    elif b < "100":
+        ("You clearly didn't listen to the instructions.")
+elif a == "Y":
+    if b >"45" and b < "70":
+        ("Welcome, Verificator. Please supervise the exam students.")
+    else: 
+        ("You may not take the exam.")
+else: 
+    print ("You are the worst impersonator I have ever seen. Please take a seat in this 100-page 30 minute medical exam.")
+
+    
+
